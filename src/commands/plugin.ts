982169a26595c7e1c4310d1cca710748021c397b
Lewis Crichton <lewi@lewisakura.moe>
2024-05-12 11:18:11 +0100

    feat: update plugin command

--- src/commands/plugin.ts
+++ src/commands/plugin.ts
@@ -2,7 +2,8 @@ import leven from "leven";
 
 import { defineCommand } from "../Command";
 import { VENCORD_SITE } from "../constants";
-import { makeCachedJsonFetch } from "../util";
+import { makeCachedJsonFetch, reply } from "../util";
+import { stripIndent } from "../util/text";
 
 interface Plugin {
     name: string;
@@ -22,8 +23,12 @@ const fetchPlugins = makeCachedJsonFetch<Plugin[]>(
 
 defineCommand({
     name: "plugin",
+    aliases: ["viewplugin", "p"],
+    description: "Provides information on a plugin",
+    usage: "<plugin name>",
+    guildOnly: true,
     rawContent: true, // since we just want the plugin name, and at least one has spaces
-    async execute(msg, query) {
+    async execute(msg, _channelId, query) {
         if (!msg.inCachedGuildChannel()) return;
 
         const plugins = await fetchPlugins();
@@ -36,46 +41,30 @@ defineCommand({
         })();
 
         if (match) {
-            return msg.channel.createMessage({
-                embeds: [
-                    {
-                        title: match.name,
-                        description: match.description,
-                        color: 0xdd7878,
-                        fields: [
-                            {
-                                name: "Authors",
-                                value: match.authors
-                                    .map(a =>
-                                        // if they don't have an ID, don't mention them
-                                        a.id ? `<@${a.id}> ${a.name}` : a.name
-                                    )
-                                    .join(", "),
-                            },
-                            {
-                                name: "Required",
-                                value: match.required ? "Yes" : "No",
-                                inline: true,
-                            },
-                            {
-                                name: "Enabled by default",
-                                value: match.enabledByDefault ? "Yes" : "No",
-                                inline: true,
-                            },
-                            {
-                                name: "Has patches",
-                                value: match.hasPatches ? "Yes" : "No",
-                                inline: true,
-                            },
-                            {
-                                name: "Has commands",
-                                value: match.hasCommands ? "Yes" : "No",
-                                inline: true,
-                            },
-                        ],
-                    },
-                ],
-            });
+            let pluginInfo = stripIndent`
+            ## [\`${match.name}\`](<https://vencord.dev/plugins/${encodeURIComponent(match.name)}>)
+            ${match.description}
+            ### Authors
+            ${match.authors
+                .map(
+                    a =>
+                        // if they don't have an ID, don't mention them
+                        `- ${a.id ? `<@${a.id}> ${a.name}` : a.name}`
+                )
+                .join("\n")}
+            `;
+
+            const abilities = stripIndent`
+                ${match.required ? "`*️⃣` required" : ""}
+                ${match.enabledByDefault ? "`✅` enabled by default" : ""}
+                ${match.hasPatches ? "`🩹` has patches" : ""}
+                ${match.hasCommands ? "`💬` has chat commands" : ""}
+            `.replace(/^\s*\n/gm, ""); // remove blanks
+
+            for (const section of [abilities])
+                if (section) pluginInfo += `\n${section}\n`;
+
+            return reply(msg, { content: pluginInfo });
         }
 
         // find plugins with similar names, in case of minor typos
@@ -85,14 +74,14 @@ defineCommand({
             .sort((a, b) => a.distance - b.distance);
 
         if (similarPlugins.length > 0) {
-            return msg.channel.createMessage({
+            return reply(msg, {
                 content: `Couldn't quite find the plugin you were looking for. Did you mean...\n${similarPlugins
                     .map(p => `- ${p.name}`)
                     .join("\n")}`,
             });
         }
 
-        return msg.channel.createMessage({
+        return reply(msg, {
             content:
                 "Couldn't find a plugin with that name, and there are no plugins with similar names.",
         });
