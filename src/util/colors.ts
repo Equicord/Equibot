3662229d8c31f2a730a64c7fc450f19c8f33f6a5
lewisakura <lewi@lewisakura.moe>
2024-09-25 11:58:10 +0100

    fix: ugly cotds this replaces the completely random hex generation with a hsl generation that has fixed saturation and lightness values. not sure if these are *quite* right yet, but they feel good so far?

--- src/util/colors.ts
+++ src/util/colors.ts
@@ -3,5 +3,26 @@ export function toHexColorString(color: number): string {
 }
 
 export function randomHexColor(): string {
-    return toHexColorString(Math.floor(Math.random() * (0xffffff + 1)));
+    return hsl2hex(Math.random() * 360, 0.8, 0.7);
+}
+
+function toHexComponent(component: number): string {
+    return Math.floor(component).toString(16).padStart(2, "0");
+}
+
+function hsl2hex(hue: number, sat: number, light: number): string {
+    const hueFixed = (360 + (hue % 360)) % 360;
+    const satFixed = Math.min(1, Math.max(0, sat));
+    const lightFixed = Math.min(1, Math.max(0, light));
+
+    let hex = "#";
+
+    for (let i = 0; i < 3; i++) {
+        const n = [0, 8, 4][i];
+        const k = (n + hueFixed / 30) % 12;
+        const a = satFixed * Math.min(lightFixed, 1 - lightFixed);
+        hex += toHexComponent((lightFixed - a * Math.max(-1.0, Math.min(k - 3.0, Math.min(9.0 - k, 1.0)))) * 255.0);
+    }
+
+    return hex;
 }
