a9b40449a04616c57052cd464182fc88bb05ba17
Vendicated <vendicated@riseup.net>
2024-10-10 19:42:29 +0200

    user helper functions

--- src/commands/info/discord-status.ts
+++ src/commands/info/discord-status.ts
@@ -1,6 +1,7 @@
 import { defineCommand } from "~/Commands";
 import { reply } from "~/util";
-import { toTitle } from "~/util/text";
+import { fetchJson } from "~/util/fetch";
+import { stripIndent, toTitle } from "~/util/text";
 
 const statusEmoji = (status: string) => {
     switch (status) {
@@ -68,65 +69,53 @@ defineCommand({
             return reply(msg, "Can't get discord status at the moment :c");
         }
 
-        const desiredComponents = components.components.filter(c => [
-            "API",
-            "Media Proxy",
-            "Push Notifications",
-            "Search",
-            "Voice",
-            "Gateway",
-        ].includes(c.name));
+        const systemStatus = components.components
+            .filter(c =>
+                [
+                    "API",
+                    "Media Proxy",
+                    "Push Notifications",
+                    "Search",
+                    "Voice",
+                    "Gateway",
+                ].includes(c.name)
+            )
+            .map(c => `${statusEmoji(c.status)} **${c.name}**: ${toTitle(c.status)}`)
+            .join("\n");
 
-        const systemStatuses = desiredComponents.map(
-            c => `${statusEmoji(c.status)} **${c.name}**: ${toTitle(c.status)}`
-        );
+        const systemOutages = incidents.incidents
+            .filter(i => i.status !== "resolved")
+            .map(i => {
+                const identifiedUpdate = i.incident_updates.find(update => update.status === "identified");
 
-        const desiredOutages = incidents.incidents.filter(
-            i => i.status !== "resolved"
-        );
-
-        const systemOutages = desiredOutages.map(i => {
-            const identifiedUpdate = i.incident_updates.find(update => update.status === "identified");
-
-            return `**Incident:** ${impactEmoji(i.impact)} ${i.name}
-**Status:** 🔴 ${toTitle(i.status)}
-**Identified At:** ${identifiedUpdate ? `<t:${Math.floor(new Date(identifiedUpdate.created_at).getTime() / 1000)}:F>` : "N/A"}
-**Last Updated:** <t:${Math.floor(new Date(i.incident_updates[0].updated_at).getTime() / 1000)}:F>\n`;
-        });
+                return stripIndent`
+                    **Incident:** ${impactEmoji(i.impact)} ${i.name}
+                    **Status:** 🔴 ${toTitle(i.status)}
+                    **Identified At:** ${identifiedUpdate ? `<t:${Math.floor(new Date(identifiedUpdate.created_at).getTime() / 1000)}:F>` : "N/A"}
+                    **Last Updated:** <t:${Math.floor(new Date(i.incident_updates[0].updated_at).getTime() / 1000)}:F>\n
+                `;
+            })
+            .join("\n");
 
         const systemOutagesText =
-            `\n\n__**Latest Outage Information**__\n${systemOutages.join("\n")}`;
-
-        const description = systemStatuses.join("\n");
+            `\n### Latest Outage Information\n${systemOutages}`;
 
         return reply(msg, {
             embeds: [{
                 title: "Discord Status",
                 color: 0x5865F2,
-                description: description + systemOutagesText,
+                description: systemStatus + systemOutagesText,
             }],
         });
     }
 });
 
 async function getDiscordStatusComponents(): Promise<DiscordComponentsResponse | null> {
-    try {
-        const response = await fetch("https://discordstatus.com/api/v2/components.json");
-        const data: DiscordComponentsResponse = await response.json() as DiscordComponentsResponse;
-        return data;
-    } catch (error) {
-        console.error("Error fetching Discord components:", error);
-        return null;
-    }
+    return fetchJson("https://discordstatus.com/api/v2/components.json")
+        .catch(e => console.error("Error fetching Discord components:", e));
 }
 
 async function getDiscordStatusIncidents(): Promise<DiscordIncdentsResponse | null> {
-    try {
-        const response = await fetch("https://discordstatus.com/api/v2/incidents.json");
-        const data: DiscordIncdentsResponse = await response.json() as DiscordIncdentsResponse;
-        return data;
-    } catch (error) {
-        console.error("Error fetching Discord incidents:", error);
-        return null;
-    }
+    return fetchJson("https://discordstatus.com/api/v2/incidents.json")
+        .catch(e => console.error("Error fetching Discord incidents:", e));
 }
