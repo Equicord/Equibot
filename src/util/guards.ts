2155ccdcc26990cc82d1ca384bc27b91456d5a71
thororen1234 <78185467+thororen1234@users.noreply.github.com>
2025-05-13 15:02:44 -0400

    Remove Unused Stuff

--- src/util/guards.ts
+++
@@ -1,7 +0,0 @@
-export function isTruthy<T>(item: T): item is Exclude<T, 0 | "" | false | null | undefined> {
-    return Boolean(item);
-}
-
-export function isNonNullish<T>(item: T): item is Exclude<T, null | undefined> {
-    return item != null;
-}
