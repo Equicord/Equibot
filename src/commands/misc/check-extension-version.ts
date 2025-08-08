67c471645506a0c67bd91375e46f42518fbcad72
thororen1234 <78185467+thororen1234@users.noreply.github.com>
2025-08-08 00:32:31 -0400

    Support Newer Release

--- src/commands/misc/check-extension-version.ts
+++ src/commands/misc/check-extension-version.ts
@@ -9,6 +9,16 @@ const VersionRe = />Version<\/div><div class="[^"]+">(\d+\.\d+\.\d+)<\/div>/;
 
 const getGithubTags = makeCachedJsonFetch<GithubTag[]>("https://api.github.com/repos/Equicord/Equicord/tags");
 
+function compareVersions(a: string, b: string) {
+    const pa = a.split(".").map(Number);
+    const pb = b.split(".").map(Number);
+    for (let i = 0; i < 3; i++) {
+        if (pa[i] > pb[i]) return 1;
+        if (pa[i] < pb[i]) return -1;
+    }
+    return 0;
+}
+
 defineCommand({
     name: "check-extension-version",
     description: "Check the version of the extension",
@@ -19,14 +29,21 @@ defineCommand({
             .then(res => res.text());
 
         const version = VersionRe.exec(res)?.[1];
-
         if (!version) return reply("Failed to look up the Equicord Chrome Extension version :( Try again later!");
 
         const [latestTag] = await getGithubTags();
+        const latestVersion = latestTag.name.replace(/^v/, "");
 
-        const message = latestTag.name === `v${version}`
-            ? `The Equicord Chrome Extension is up to date! (v${version})`
-            : `The Equicord Chrome Extension is out of date! (v${version} vs ${latestTag.name})`;
+        const cmp = compareVersions(version, latestVersion);
+
+        let message;
+        if (cmp === 0) {
+            message = `The Equicord Chrome Extension is up to date! (v${version})`;
+        } else if (cmp > 0) {
+            message = `The Equicord Chrome Extension is newer than the latest GitHub release! (v${version} vs v${latestVersion})`;
+        } else {
+            message = `The Equicord Chrome Extension is out of date! (v${version} vs v${latestVersion})`;
+        }
 
         reply(message);
     }
