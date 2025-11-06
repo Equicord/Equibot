cda3b332df96064fbe81c309fcc4d2fde5824d44
thororen1234 <78185467+thororen1234@users.noreply.github.com>
2025-11-06 11:21:45 -0500

    fix url

--- src/util/git.ts
+++ src/util/git.ts
@@ -1,11 +1,6 @@
-import { execFileP } from "~/util/childProcess";
 
 import { makeLazy } from "./lazy";
 
 export const getGitRemote = makeLazy(async () => {
-    const { stdout } = await execFileP("git", ["remote", "get-url", "origin"]);
-    return stdout
-        .trim()
-        .replace(/\.git$/, "")
-        .replace(/^git@(.+?):/, "https://$1/");
+    return "https://github.com/Equicord/Equibot";
 });
