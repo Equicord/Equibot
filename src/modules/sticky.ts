40a82fac0dd22fd8da1f77bfa8da3edac404794e
M <91066601+mishl-dev@users.noreply.github.com>
2026-02-07 05:17:31 +0600

    Enable notification suppression for sticky messages
    Add flag to suppress notifications for sticky messages.

--- src/modules/sticky.ts
+++ src/modules/sticky.ts
@@ -44,6 +44,7 @@ export class StickyState {
 
         const message = await Vaius.rest.channels.createMessage(this.channelId, {
             content: `${content}\n-# This is an automated sticky message.`,
+            flags: [4096], // MessageFlags.SuppressNotifications https://discord.js.org/docs/packages/core/main/MessageFlags:Enum#SuppressNotifications
             allowedMentions: {
                 everyone: false,
                 roles: [],
