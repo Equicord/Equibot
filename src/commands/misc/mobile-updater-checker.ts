0bb52e078718418586c582141640df129a2d1ac9
thororen1234 <78185467+thororen1234@users.noreply.github.com>
2026-03-10 12:10:18 -0400

    Update mobile-updater-checker.ts

--- src/commands/misc/mobile-updater-checker.ts
+++ src/commands/misc/mobile-updater-checker.ts
@@ -2,7 +2,6 @@ import { defineCommand } from "~/Commands";
 import Config from "~/config";
 import { checkAndroid } from "~/modules/updateTracker/android";
 import { checkIos } from "~/modules/updateTracker/ios";
-import { reply } from "~/util/discord";
 
 defineCommand({
     enabled: Config.updateTracker.enabled,
@@ -16,7 +15,5 @@ defineCommand({
     async execute({ msg }) {
         if (Config.updateTracker.android) await checkAndroid(true, msg.channelID);
         if (Config.updateTracker.ios) await checkIos(true, msg.channelID);
-
-        reply(msg, "Now checking for new mobile releases!");
     },
 });
