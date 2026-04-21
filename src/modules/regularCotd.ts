f03c5495485a86e2b642e29c2d4fde03302d4351
thororen1234 <78185467+thororen1234@users.noreply.github.com>
2026-04-21 15:45:32 -0400

    oops

--- src/modules/regularCotd.ts
+++ src/modules/regularCotd.ts
@@ -72,6 +72,32 @@ function isDark(hex) {
     return brightness < 128;
 }
 
+export async function drawBlobCatCozy(color: string, size = 512) {
+    color = String(color)
+        .replaceAll("&", "&amp;")
+        .replaceAll("<", "&lt;")
+        .replaceAll(">", "&gt;")
+        .replaceAll("\"", "&quot;")
+        .replaceAll("'", "&#039;");
+
+    const base = join(ASSET_DIR, "image-gen/regular-icon");
+    const svgPath = join(base, "bcc.svg");
+
+    const svgData = await readFile(svgPath, "utf-8");
+    const tintedSvg = svgData.replaceAll("#1a2b3c", color);
+    const svg = await loadImage(Buffer.from(tintedSvg));
+
+    svg.width = size;
+    svg.height = size;
+
+    const canvas = createCanvas(size, size);
+
+    const ctx = canvas.getContext("2d");
+    ctx.drawImage(svg, 0, 0, size, size);
+
+    return canvas.toBuffer("image/png");
+}
+
 export async function drawRoleIcon(color: string, size = 512) {
     color = String(color)
         .replaceAll("&", "&amp;")
