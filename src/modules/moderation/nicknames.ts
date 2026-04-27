95f2020c022e8cf5df06e6a7278a8975fceb3296
Luqe <luqenov@tutamail.com>
2026-04-27 20:31:16 -0300

    feat(moderation): skip nickname moderation for team and mod

--- src/modules/moderation/nicknames.ts
+++ src/modules/moderation/nicknames.ts
@@ -1,9 +1,13 @@
 import { Member } from "oceanic.js";
 import { Vaius } from "~/Client";
 import { silently } from "~/util/functions";
+import Config from "~/config";
 
 export async function moderateNick(member: Member) {
     if (member.bot || !member.guild.permissionsOf(Vaius.user.id).has("MANAGE_NICKNAMES")) return;
+
+    const ignoredRoles = [Config.roles.team, Config.roles.mod];
+    if (member.roles.some(role => ignoredRoles.includes(role))) return;
 
     const name = member.displayName;
     const normalizedName = name
