1ce4010417fed4a526890fc8e2fcb4fade4b3089
thororen1234 <78185467+thororen1234@users.noreply.github.com>
2026-08-09 21:04:07 -0400

    inline

--- src/commands/info/sponsor.ts
+++ src/commands/info/sponsor.ts
@@ -7,9 +7,31 @@ defineCommand({
     usage: null,
     async execute({ reply }) {
         return reply(
-            "You can support Equicord and related projects by donating here: https://github.com/sponsors/thororen1234" +
+            "## $1 a month" +
             "\n" +
-            "If you can't use GitHub for donations please make a ticket here: https://discord.com/channels/1173279886065029291/1300883665244389478"
+            "- You'll get your GitHub profile featured on all Equicord related repositories." +
+            "\n" +
+            "- You'll get a donator Equicord profile badge (non-custom) (visible to all Equicord users)" +
+            "\n" +
+            "- You'll get a Sponsor badge on your GitHub profile." +
+            "\n" +
+            "## $5 a month" +
+            "\n" +
+            "* All previous tier perks." +
+            "\n" +
+            "* Custom Equicord profile badge." +
+            "\n" +
+            "  * This badge is visible to Equicord users and people who use global badges on the various platforms" +
+            "\n" +
+            "  * **Badge Design Rules:**" +
+            "\n" +
+            '    * Cannot be blue or resemble corporate icons (Roblox, X/Twitter, etc.), nor claim roles like "Owner", "Developer/Maintainer" or "Staff/Admin/Moderator". Unique shapes and other colors are allowed.' +
+            "\n" +
+            "    * No NSFW content, existing Discord or client mod badges (Vencord, BD, RePlugged, etc.), or offensive symbols (swastikas, etc.)." +
+            "\n" +
+            "* For an additional $3, you'll get another custom badge (up to 5 total)." +
+            "\n\n" +
+            "Donate: https://github.com/sponsors/thororen1234"
         );
 
     }
